1b43aadcbac32b8285a61babba0ae20c0e830700
Faisal Faiz <faisalfaizsiddiqui@gmail.com>
2025-08-20 02:47:21 +0530

    chore: remove lightningcss dependency and add TypeScript support

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,18 @@
 // const nextConfig = {};
 
 // export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    turbo: true, // if using turbopack
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     turbo: true, // if using turbopack
+//   },
+// }
+
+// export default nextConfig
+
+export default {
+  turbopack: {
+    // You can put options here later if needed
   },
-}
-
-export default nextConfig
-
+};
